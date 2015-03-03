b50b25c99c165fba1f6483e8c3caf9953cacdcb1
Christoph Merkelbach <upsworld@gmx.de>
2015-03-03 19:57:22 +0100

    some refactoring and fixing for frontend movement, backend still needs fixing

--- app/assets/javascript/pageflow/linkmap_page/page_type.js
+++ app/assets/javascript/pageflow/linkmap_page/page_type.js
@@ -5,18 +5,7 @@ pageflow.pageType.register('linkmap_page', _.extend({
   enhance: function(pageElement, configuration) {
     this.content = pageElement.find('.scroller');
     this.panorama = pageElement.find('.linkmap');
-    this.panorama.linkmapPanorama();
-//    this.panorama.linkmapPanorama("goTo", configuration.panorama_initial_position);
-
-    var that = this;
-    this.panorama.find('img').load(function() {
-      that.content.scroller('refresh');
-    });
-
-    window.myscroller = that.content.scroller('returnScroller');
-
-    this.scroller = that.content.scroller('returnScroller');
-
+    this.content.linkmapPanorama({page: pageElement});
 
     this.linkmapAreas = pageElement.find('.linkmap_areas');
     this.linkmapAreas.linkmap({
@@ -25,71 +14,6 @@ pageflow.pageType.register('linkmap_page', _.extend({
 
     this.setupPageLinkAreas(pageElement);
     this.setupAudioFileAreas(pageElement);
-
-    var scrollTimer,
-      containerWidth = this.content.width(),
-      containerHeight = this.content.height(),
-      margin = 0.2,
-      speedX = 0,
-      speedY = 0,
-      speedUp = 30,
-      faktorX = 0,
-      faktorY = 0;
-
-
-
-    this.content.on('mouseenter', function() {
-      speedX = 0;
-      speedY = 0;
-
-      newTimer();
-    });
-
-    this.content.on('mouseleave', function() {
-      speedX = 0;
-      speedY = 0;
-
-      clearInterval(scrollTimer);
-    });
-
-    this.content.on('mousemove', function(e) {
-
-      if(!scrollTimer) {newTimer();};
-      containerWidth = that.content.width(),
-      containerHeight = that.content.height();
-
-      var percentagePositionX = (e.pageX - containerWidth/2) / (containerWidth/2),
-        percentagePositionY = (e.pageY - containerHeight/2) / (containerHeight/2),
-        onOuterAreasX = (percentagePositionX > 1 - margin*2 || percentagePositionX < -1 + margin*2),
-        onOuterAreasY = (percentagePositionY > 1 - margin*2 || percentagePositionY < -1 + margin*2);
-
-      faktorX = percentagePositionX > 0 ? 1 : -1;
-      faktorY = percentagePositionY > 0 ? 1 : -1;
-
-      speedX = onOuterAreasX ? (percentagePositionX - ((1-margin*2)*(faktorX))) / (margin*2) : 0;
-      speedY = onOuterAreasY ? (percentagePositionY - ((1-margin*2)*(faktorY))) / (margin*2) : 0;
-    });
-
-    function newTimer() {
-      scrollTimer = setInterval(function() {
-        var scrollX = -speedX * speedUp;
-        var scrollY = -speedY * speedUp;
-
-        if(that.scroller.x + scrollX > 0 || that.scroller.x + scrollX < that.scroller.maxScrollX) {
-          scrollX = 0;
-        }
-        if(that.scroller.y + scrollY > 0 || that.scroller.y + scrollY < that.scroller.maxScrollY) {
-          scrollY = 0;
-        }
-
-        that.scroller.scrollBy(scrollX, scrollY);
-
-//          that.scroller.x > 0 && that.scroller.x < that.scroller.maxScrollX? scrollX : 0,
-//          that.scroller.y > 0 && that.scroller.y < that.scroller.maxScrollY? scrollY : 0);
-      },25);
-    }
-
-
   },
 
   setupPageLinkAreas: function(pageElement) {
@@ -125,8 +49,7 @@ pageflow.pageType.register('linkmap_page', _.extend({
   },
 
   resize: function(pageElement, configuration) {
-   // this.panorama.linkmapPanorama('refresh');
-    //this.linkmapAreas.linkmap('refresh');
+    this.content.linkmapPanorama("refresh");
   },
 
   prepare: function(pageElement, configuration) {
@@ -156,14 +79,25 @@ pageflow.pageType.register('linkmap_page', _.extend({
     pageElement.find('h2 .subtitle').text(configuration.get('subtitle') || '');
     pageElement.find('.contentText p').html(configuration.get('text') || '');
 
-    this.updateCommonPageCssClasses(pageElement, configuration);
-
     pageElement.find('.shadow').css({
       opacity: configuration.get('gradient_opacity') / 100
     });
 
+    if(pageElement.find('.panorama').length == 0) {
+      pageElement.find('.linkmap').prepend('<img class="panorama" />');
+    }
+
+    this.updateCommonPageCssClasses(pageElement, configuration);
+
+    var panorama = pageElement.find('.panorama'),
+    panoramaImageRatio = panorama.attr('data-width') / panorama.attr('data-height');
+    console.log(panoramaImageRatio, pageElement.height() * 1.2);
+
+    panorama.height(pageElement.height() * 1.2);
+    panorama.width(panorama.height() * panoramaImageRatio);
+
     //this.panorama.linkmapPanorama('refresh');
-    this.updateLinkmapAfterEmbeddedViewsHaveBeenUpdated();
+    //this.updateLinkmapAfterEmbeddedViewsHaveBeenUpdated();
 
     if(configuration.get('panorama_initial_position') != this.lastPanoramaPosition) {
      // this.panorama.linkmapPanorama('goTo', configuration.get('panorama_initial_position'));
@@ -174,7 +108,7 @@ pageflow.pageType.register('linkmap_page', _.extend({
 
   updateLinkmapAfterEmbeddedViewsHaveBeenUpdated: function() {
     setTimeout(_.bind(function() {
-      //this.linkmapAreas.linkmap('refresh');
+      this.linkmapAreas.linkmap('refresh');
     }, this), 1000);
   }
 }, pageflow.commonPageCssClasses));
--- app/assets/javascript/pageflow/linkmap_page/panorama_widget.js
+++ app/assets/javascript/pageflow/linkmap_page/panorama_widget.js
@@ -2,6 +2,99 @@
   $.widget('pageflow.linkmapPanorama', {
     _create: function() {
 
+      var that = this,
+        pageElement = this.options.page;
+
+      this.img = this.element.find('img');
+
+      this.img.load(function() {
+        that.content.scroller('refresh');
+      });
+
+      this.refresh();
+
+      window.myscroller = this.element.scroller('returnScroller');
+
+      this.scroller = this.element.scroller('returnScroller');
+
+      var scrollTimer,
+        containerWidth = this.element.width(),
+        containerHeight = this.element.height(),
+        margin = 0.2,
+        speedX = 0,
+        speedY = 0,
+        speedUp = 30,
+        faktorX = 0,
+        faktorY = 0,
+        drag = false;
+
+      this.element.on('mousedown touchstart', function () {
+        drag = true;
+      });
+
+      this.element.on('mouseup touchend', function () {
+        drag = false;
+      });
+
+      this.element.on('mouseenter', function() {
+        speedX = 0;
+        speedY = 0;
+
+        newTimer();
+      });
+
+      this.element.on('mouseleave', function() {
+        speedX = 0;
+        speedY = 0;
+
+        clearInterval(scrollTimer);
+      });
+
+      this.element.on('mousemove', function(e) {
+
+        if(!scrollTimer) {newTimer();};
+        containerWidth = that.element.width(),
+        containerHeight = that.element.height();
+
+        var percentagePositionX = (e.pageX - containerWidth/2) / (containerWidth/2),
+          percentagePositionY = (e.pageY - containerHeight/2) / (containerHeight/2),
+          onOuterAreasX = (percentagePositionX > 1 - margin*2 || percentagePositionX < -1 + margin*2),
+          onOuterAreasY = (percentagePositionY > 1 - margin*2 || percentagePositionY < -1 + margin*2);
+
+        faktorX = percentagePositionX > 0 ? 1 : -1;
+        faktorY = percentagePositionY > 0 ? 1 : -1;
+
+        speedX = onOuterAreasX ? (percentagePositionX - ((1-margin*2)*(faktorX))) / (margin*2) : 0;
+        speedY = onOuterAreasY ? (percentagePositionY - ((1-margin*2)*(faktorY))) / (margin*2) : 0;
+      });
+
+      function newTimer() {
+        scrollTimer = setInterval(function() {
+          var scrollX = -speedX * speedUp;
+          var scrollY = -speedY * speedUp;
+
+          if(that.scroller.x + scrollX > 0) {
+            scrollX = -that.scroller.x;
+          }
+          if(that.scroller.x + scrollX < that.scroller.maxScrollX) {
+            scrollX = that.scroller.maxScrollX - that.scroller.x;
+          }
+          if(that.scroller.y + scrollY > 0) {
+            scrollY = -that.scroller.y;
+          }
+          if(that.scroller.y + scrollY < that.scroller.maxScrollY) {
+            scrollY = that.scroller.maxScrollY - that.scroller.y;
+          }
+
+          if(!drag) {
+            that.scroller.scrollBy(scrollX, scrollY);
+          }
+
+        },25);
+      }
+    },
+    refresh: function() {
+      this.img.height(this.options.page.height() * 1.2);
     }
   });
 }(jQuery));
