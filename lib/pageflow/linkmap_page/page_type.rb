fad1df7719521cfb3fdc55764c68a9ffc39bd056
Tim Fischbach <tfischbach@codevise.de>
2015-03-11 19:34:57 +0100

    Use video as thumbnail candidate

--- lib/pageflow/linkmap_page/page_type.rb
+++ lib/pageflow/linkmap_page/page_type.rb
@@ -6,6 +6,14 @@ module Pageflow
       def view_helpers
         [ImageTagHelper, AreasHelper]
       end
+
+      def thumbnail_candidates
+        [
+          {attribute: 'thumbnail_image_id', file_collection: 'image_files'},
+          {attribute: 'background_image_id', file_collection: 'image_files'},
+          {attribute: 'background_video_id', file_collection: 'video_files'}
+        ]
+      end
     end
   end
 end
