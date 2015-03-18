3024091640d912fbe3f337e3af24c77ef98ed18b
Christoph Merkelbach <cmerkelbach@codevise.de>
2015-03-18 18:11:52 +0100

    adds gyro.js

--- app/assets/javascript/pageflow/linkmap_page.js
+++ app/assets/javascript/pageflow/linkmap_page.js
@@ -2,5 +2,6 @@
 //= require ./linkmap_page/panorama_widget
 //= require ./linkmap_page/linkmap_widget
 //= require ./linkmap_page/page_type
+//= require ./linkmap_page/gyro
 
 pageflow.linkmapPage = pageflow.linkmapPage || {};
