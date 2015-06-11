9df625883803bcdc91172c289489164760c409ef
Tim Fischbach <tfischbach@codevise.de>
2015-06-11 13:05:38 +0200

    Move Widgets to Directory

--- app/assets/javascript/pageflow/linkmap_page.js
+++ app/assets/javascript/pageflow/linkmap_page.js
@@ -1,9 +1,7 @@
 //= require_self
 //= require ./linkmap_page/features
-//= require ./linkmap_page/panorama_widget
-//= require ./linkmap_page/linkmap_widget
 //= require ./linkmap_page/page_type
-//= require ./linkmap_page/gyro
+//= require ./linkmap_page/vendor/gyro
 //= require_tree ./linkmap_page/widgets
 
 pageflow.linkmapPage = pageflow.linkmapPage || {};
